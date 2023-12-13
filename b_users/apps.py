4abc6b4724d41b7f2b7480d62bf7452cb6f08b2d
Oleksandr Khomenko <khomedich@gmail.com>
2023-12-13 12:14:32 +0200

    rest_framework_simplejwt auth

--- b_users/apps.py
+++ b_users/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BUsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'b_users'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "b_users"
